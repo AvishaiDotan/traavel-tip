b4824a484fa9411eca79acce9a6b24a13a128d12
Avishai Dotan <avishaiDotan@gmail.com>
2022-11-02 17:31:33 +0200

    added features

--- js/app.controller.js
+++ js/app.controller.js
@@ -7,23 +7,49 @@ window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 window.onSendSearch = onSendSearch;
+window.onCopyAddress = onCopyAddress
 
 let markers = [];
 
 
 function onInit() {
     const Location = { lat: 29.5577, lng: 34.9519 };
-
-    mapService.initMap().then(() => { console.log('Map is ready') })
+    mapService.initMap()
+        .then(renderSavedLocations)
         .catch(() => console.log('Error: cannot init map'))
         
+}
+
+function renderApp() {
+    mapService.renderMarkers()
+        .then(renderSavedLocations)
+}
+
+
+function renderSavedLocations(locations) {
+    let strHTMLs = locations.map(({id ,name, createdAt, lat, lng}) => `
+                <article>
+                    <h2>${name}</h2>
+                    <p>${createdAt}</p>
+                    <p>${lat}, ${lng}</p>
+                    <div class="location-actions">
+                        <button onclick="onPanTo(${lat}, ${lng})" class="goto-action" >Go</button>
+                        <button onclick="onDeleteLoc('${id}')" class="delete-action">Delete</button>
+                    </div>
+                </article>
+    `)
+
+    window.onPanTo = onPanTo
+    window.onDeleteLoc = onDeleteLoc
+
+    document.querySelector('.saved-loc-container').innerHTML = strHTMLs.join('')
 }
 
 
 
 function onAddMarker(position) {
-    console.log('Adding a marker', position);
     mapService.addMarker(position)
+    renderApp()
 }
 
 function onGetLocs(ev) {
@@ -36,39 +62,43 @@ function onGetLocs(ev) {
 }
 
 function onGetUserPos() {
-    getPosition().then(pos => {
-        console.log('User position is:', pos.coords)
-        document.querySelector('.user-pos').innerText =
-            `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-    })
+    getPosition()
+        .then(pos => {
+            document.querySelector('.user-pos').innerText = 
+                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+            mapService.panTo(pos.coords.latitude, pos.coords.longitude)
+        })
         .catch(err => {
             console.log('err!!!', err)
         })
 }
 
-function onPanTo() {
-    console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+function onPanTo(lat, lng) {
+    mapService.panTo(lat, lng)
 }
 
 function onSendSearch(val) {
     mapService.sendLocation(val)
     document.querySelector('.user-pos').innerText = val.toUpperCase()
-    
-
 }
 
-// function onPanTo() {
-//     console.log('Panning the Map')
-//     mapService.panTo(35.6895, 139.6917)
-// }
+function onDeleteLoc(locId) {
+    locService.deleteLoc(locId)
+    renderApp()
+}
 
-
+function onCopyAddress() {
+    getPosition()
+        .then((pos) => {
+            const address = `https://github.io/me/travelTip/index.html?lat=${pos.coords.latitude}&lng=${pos.coords.longitude}` 
+            navigator.clipboard.writeText(address);
+        })
+    
+}
 
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos')
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -5,7 +5,8 @@ export const mapService = {
     initMap,
     addMarker,
     panTo,
-    sendLocation
+    sendLocation,
+    renderMarkers,
 }
 
 
@@ -25,18 +26,19 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
 }
 
 function renderMarkers() {
-    locService.getLocs()
+    return locService.getLocs()
         .then(markers =>
-            markers.forEach(({ title, lat, lng }) => {
-                new google.maps.Marker({
-                    position: { lat, lng },
-                    map: gMap,
-                    title: title,
+            {
+                markers.forEach(({ title, lat, lng }) => {
+                    new google.maps.Marker({
+                        position: { lat, lng },
+                        map: gMap,
+                        title: title,
+                    })
                 })
-                return Promise.resolve(locService.getLocs())
-            })
-        )
 
+                return Promise.resolve(markers)
+            })
 }
 
 function addMarker(loc) {
@@ -57,7 +59,6 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    console.log('gg');
     const API_KEY = `AIzaSyAxdXNT9j1GWz7kU5pwAK8bMSR2OJ0Bg6Q` //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
@@ -86,5 +87,5 @@ function _addListener() {
         onAddMarker(event.latLng);
     });
 
-    return Promise.resolve('')
+    return Promise.resolve()
 }
